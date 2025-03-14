d45fc45cc15b2b5bb54226d00d15e07170e28a3d
Rathanon Makaramanee <rathanon.makaramanee@gmail.com>
2025-03-14 10:55:17 +0700

    Reapply "Class 06 Done."
    This reverts commit e2eb20edcf7e7653e718e1bba02ac2ae6b976c53.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import InteractiveCard from './InteractiveCard';
 
-export default function ProductCard({carName,imgSrc}:{carName:string,imgSrc:string}){
+export default function ProductCard({carName,imgSrc,onCompare}:{carName:string,imgSrc:string,onCompare:Function}){
     return (
         <InteractiveCard contentName={carName}>    
             <div className='w-full h-[70%] relative rounded-t-lg'>
@@ -10,7 +10,9 @@ export default function ProductCard({carName,imgSrc}:{carName:string,imgSrc:stri
                 fill = {true}
                 className='object-cover rounded-t-lg'/>
             </div>
-            <div className='w-full h-[30%] p-[10px]'>{carName}</div>
+            <div className='w-full h-[15%] p-[10px]'>{carName}</div>
+            <button className='block h-[10%] tex-sm rounded-md bg-sky-600 hover:bg-indigo-600 mx-2 px-1 py-1 text-white shadow-sm'
+            onClick={(e)=>{e.stopPropagation(); onCompare(carName)}}>Compare</button>
         </InteractiveCard>
     )
 }
